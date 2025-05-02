14965b1dd520abdb37249b611f7e99aca5e8d8b9
Hakkimhong12 <kimhongs200@gmail.com>
2025-05-03 06:50:17 +0700

    'commit'

--- application.py
+++ application.py
@@ -43,4 +43,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
